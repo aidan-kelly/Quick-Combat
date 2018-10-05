37ddccdd83493159593f90e9bd8ecb5fd4d830c6
Aidan Kelly <it.is.aidan@gmail.com>
2018-10-04 23:30:15 -0600

    Enemies added to list now appear in ListView

--- app/src/main/java/net/aidanjameskelly/quickcombat/CreateEncounterActivity.java
+++ app/src/main/java/net/aidanjameskelly/quickcombat/CreateEncounterActivity.java
@@ -3,22 +3,36 @@ package net.aidanjameskelly.quickcombat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class CreateEncounterActivity extends AppCompatActivity {
 
     EditText enemyNameEditText;
+    ListView listView;
+    ArrayList<String> listOfEnemies;
+    ArrayAdapter<String> ourAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_encounter);
+        listView = findViewById(R.id.listView);
+        listOfEnemies = new ArrayList<String>();
 
         enemyNameEditText = findViewById(R.id.enemyNameEditText);
+
+        ourAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfEnemies);
+        listView.setAdapter(ourAdapter);
     }
 
     public void addToList(View view){
+        listOfEnemies.add(enemyNameEditText.getText().toString());
+        ourAdapter.notifyDataSetChanged();
         Toast.makeText(this, "Adding " + enemyNameEditText.getText() + " to the list!", Toast.LENGTH_SHORT).show();
     }
 
