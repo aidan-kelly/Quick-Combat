73d50876a7da5a13b8343b50863227f9aa86339f
Aidan Kelly <it.is.aidan@gmail.com>
2018-10-31 11:14:56 -0600

    Slowly adding database support

--- app/src/main/java/net/aidanjameskelly/quickcombat/Encounter.java
+++ app/src/main/java/net/aidanjameskelly/quickcombat/Encounter.java
@@ -1,4 +1,16 @@
 package net.aidanjameskelly.quickcombat;
 
 public class Encounter {
+
+    public static final String TABLE_NAME = "encounters";
+
+    public static final String COLUMN_ENCID = "encID";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PLAYERS = "numOfPlayers";
+
+    private int encID;
+    private String name;
+    private int numOfPlayers;
+
+
 }
--- app/src/main/java/net/aidanjameskelly/quickcombat/Enemy.java
+++ app/src/main/java/net/aidanjameskelly/quickcombat/Enemy.java
@@ -1,4 +1,18 @@
 package net.aidanjameskelly.quickcombat;
 
 public class Enemy {
+
+    public static final String TABLE_NAME = "enemies";
+
+    public static final String COLUMN_ENCID = "encID";
+    public static final String COLUMN_ENEID = "eneID";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_HP = "hp";
+    public static final String COLUMN_DEX = "dex";
+
+    private int encID;
+    private int eneID;
+    private String name;
+    private int hp;
+    private int dex;
 }
